464872751a82088e952a3963bff8738da8407ace
SEVRVS <duquesney.Edmond14@gmail.com>
2021-11-29 14:21:43 +0100

    Calculatrice fonctionnelle

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var screen = document.getElementById("screen");
 var historique = document.getElementById("historique");
 // Récupération de la variable local historique qui contient la class display à associer.
 var dChoice = localStorage.getItem("historique");
-historique.setAttribute('class', dChoice);
+historique.setAttribute("class", dChoice);
 
 boutons.map((btn) => {
   btn.addEventListener("click", () => {
@@ -15,6 +15,23 @@ boutons.map((btn) => {
       // Si l'utilisateur clique sur la touche C, clear l'affichage de l'écran
       case "clear":
         screen.value = "";
+        break;
+      // Au clique sur la touche =
+      case "=":
+        // Je test si la fonction eval ne detecte pas d'erreur
+        try {
+          //Si tout est bon
+          var result = eval(screen.value);
+          var calculeStr = screen.value + "=" + result;
+          screen.value = result;
+        } catch {
+          // Si une erreure est détectée.
+          btn.disabled = "true";
+          screen.value = "Fatal error..";
+          setTimeout(() => {
+            location.reload();
+          }, 1000);
+        }
         break;
       //Affichage de l'historique (localstorage pour garder le choix d'affichage)
       case "historique":
@@ -28,7 +45,7 @@ boutons.map((btn) => {
             break;
           case "d-block":
             localStorage.setItem("historique", "d-none");
-            historique.setAttribute('class', 'd-none')
+            historique.setAttribute("class", "d-none");
             break;
         }
         break;
